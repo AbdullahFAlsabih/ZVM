09e4d86b14b45c90308a7ccc9d71f844a0096ea2
Abdullah Alsabih <aalsabih@outlook.com>
2026-06-21 10:05:47 +0300

    Add files via upload

---
+++ zvm_instruction.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include "zvm_instruction.h"
+#include "zvm.h"
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(add)
+    vm->cpu.R[output] = vm->cpu.R[left] + vm->cpu.R[right];
+    printf("R=%u\n", vm->cpu.R[output]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(sub)
+    vm->cpu.R[output] = vm->cpu.R[left] - vm->cpu.R[right];
+    printf("R=%u\n", vm->cpu.R[output]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(ldi)
+    vm->cpu.R[left] = right;
+    printf("R=%u\n", vm->cpu.R[left]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(str)
+    /* STR reg, mem */
+    vm->program.data[right] = vm->cpu.R[left];
+    printf("MEM=%u\n", vm->program.data[right]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(ldm)
+    /* LDM reg, mem */
+    vm->cpu.R[left] = vm->program.data[right];
+    printf("R=%u\n", vm->cpu.R[left]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(lda)
+    uint8_t address = vm->cpu.R[left];
+    if(!zvm_is_address(address)){
+        zvm_raise(vm, EXECUTE, BAD_MEMORY_ADDRESS)
+        return false;
+    }
+    vm->cpu.R[right] = vm->program.data[address];
+    printf("R=%u\n", vm->cpu.R[right]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(sta)
+    /* STA reg, reg */
+    uint8_t address = vm->cpu.R[left];
+    if(!zvm_is_address(address)){
+        zvm_raise(vm, EXECUTE, BAD_MEMORY_ADDRESS)
+        return false;
+    }
+    vm->program.data[address] = vm->cpu.R[right];
+    printf("address=%u\n", vm->program.data[address]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(push)
+    uint8_t value;
+    if(vm->cpu.SP >= ZVM_PROGRAM_DEFAULT_STACK_SEGMENT_SIZE){
+        zvm_raise(vm, EXECUTE, STACK_OVERFLOW)
+        return false;
+    }
+
+    vm->cpu.SP++;
+    value = vm->cpu.R[left];
+    vm->program.stack[(ZVM_PROGRAM_DEFAULT_STACK_SEGMENT_SIZE - 1) - vm->cpu.SP] = value;
+    printf("stack=%u\n", value);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(pop)
+    uint8_t value;
+    if(vm->cpu.SP < 0){
+        zvm_raise(vm, EXECUTE, STACK_UNDERFLOW)
+        return false;
+    }
+
+    value = vm->program.stack[(ZVM_PROGRAM_DEFAULT_STACK_SEGMENT_SIZE - 1) - vm->cpu.SP];
+    vm->cpu.SP--;
+    
+    vm->cpu.R[left] = value;
+    printf("R=%u\n", vm->cpu.R[left]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(inc)
+    /* INC reg*/
+    vm->cpu.R[left]++;
+    printf("R=%u\n", vm->cpu.R[left]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(dec)
+    /* DEC reg*/
+    vm->cpu.R[left]--;
+    printf("R=%u\n", vm->cpu.R[left]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(and)
+    vm->cpu.R[output] = vm->cpu.R[left] & vm->cpu.R[right];
+    printf("R=%u\n", vm->cpu.R[output]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(or)
+    vm->cpu.R[output] = vm->cpu.R[left] | vm->cpu.R[right];
+    printf("R=%u\n", vm->cpu.R[output]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(xor)
+    vm->cpu.R[output] = vm->cpu.R[left] ^ vm->cpu.R[right];
+    printf("R=%u\n", vm->cpu.R[output]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(not)
+  vm->cpu.R[right] = ~ vm->cpu.R[left];
+    printf("R=%u\n", vm->cpu.R[right]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(shl)
+uint8_t shift_amount = vm->cpu.R[right];
+if(shift_amount >7){
+        zvm_raise(vm, EXECUTE, INVALID_SHIFT_AMOUNT);
+        return false;
+}
+   vm->cpu.R[output] = vm->cpu.R[left] << vm->cpu.R[right];
+    printf("R=%u\n", vm->cpu.R[output]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(shr)
+uint8_t shift_amount = vm->cpu.R[right];
+if(shift_amount >7){
+        zvm_raise(vm, EXECUTE, INVALID_SHIFT_AMOUNT);
+        return false;
+}
+    vm->cpu.R[output] = vm->cpu.R[left] >> vm->cpu.R[right];
+    printf("R=%u\n", vm->cpu.R[output]);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
+
+ZVM_INSTRUCTION_HANDLER_FUNCTION_BEGIN(jmp)
+uint8_t target_address = vm->cpu.R[left];
+if(target_address >= ZVM_PROGRAM_DEFAULT_CODE_SEGMENT_SIZE){
+ zvm_raise(vm, EXECUTE, BAD_MEMORY_ADDRESS);
+        return false;
+}
+    vm->cpu.IP = target_address;
+printf("JUMPED TO IP=%u\n", vm->cpu.IP);
+ZVM_INSTRUCTION_HANDLER_FUNCTION_END
